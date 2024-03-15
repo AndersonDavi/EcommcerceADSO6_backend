cef405d5e084f82210b1f1f64c4a30b5164879c8
unknown <andesondavid17@gmail.com>
2024-03-14 19:51:28 -0500

    build: server certificate

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +8,5 @@ public class DemoApplication  {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-//    public static Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        LOG.info("Ola");
-//        LOG.warn("Ola");
-//    }
 
 }
